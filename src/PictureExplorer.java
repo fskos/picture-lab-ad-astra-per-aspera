d6a61ebe09918555cb3ce0075d9c9c5e261c660b
90300496 <90300496@192.168.1.28>
2017-03-26 15:40:15 -0500

    1. 2. and 3. on page 9 done

--- src/PictureExplorer.java
+++ src/PictureExplorer.java
@@ -802,7 +802,7 @@ public class PictureExplorer implements MouseMotionListener, ActionListener, Mou
    */  
   public static void main( String args[])
   {
-    Picture pix = new Picture("beach.jpg");
+    Picture pix = new Picture("beachCopy.jpg");
     pix.explore();
   }
   
